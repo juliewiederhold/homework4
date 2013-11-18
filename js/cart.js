e287beae67c5978cf71eeb47d6e2760a1b1ce969
unknown <iguest@is-m440m27-21.ischool.uw.edu>
2013-11-18 14:12:31 -0800

    11/18 - have basic template for cart. Need to figure out how to get menu.js into JSON

---
+++ js/cart.js
@@ -0,0 +1,31 @@
+/*
+    createCartModel()
+
+    Creates a model for the shopping cart. This uses the ListModel
+    as the prototype, but adds a few specific methods.
+
+    The config parameter can contain the following properties:
+    - items (array of objects) initial items for the cart (optional)
+*/
+
+function createCartModel(config) {
+	var model = createListModel(config);
+
+    // returns the total price of the elements in the cart
+	model.getTotalPrice = function() {
+    	var idx;
+    	var totalPrice = 0;
+
+    	for(idx = 0; idx < this.items.length; ++idx){
+    		totalPrice += this.items[idx].price;
+    	}
+    	return totalPrice.toFixed(2);
+	}; //getTotalPrice()
+
+    // returns a JSON representation of the cart items
+	model.toJSON = function() {
+		return JSON.stringify(this.items);
+	};
+
+	return model;
+} //createCartModel()
---
+++ js/controller.js
@@ -0,0 +1,20 @@
+$(function(){
+	var formatLabels = {
+		prices[0]: 'Small',
+		prices[1]: 'Medium',
+		prices[2]: 'Large'
+	}
+
+	var cartModel = createCartModel();
+
+	var cartView = createCartView({
+		model: cartModel,
+		template: $('.cart-item-template'),	// all of these are defined in index.html
+		container: $('.cart-item-container'),
+		totalPrice: $('.total-price')
+	});
+
+	var menuModel = createMenuModel({
+		url: 
+	})
+});
